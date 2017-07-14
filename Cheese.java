c021c8239f1d0352fbbaa08bc14c24ddddf59ab4
kimlundin <klundin@twu.edu>
2017-07-14 15:19:29 -0500

    Test 1
    These files were the result of a Test

---
+++ Cheese.java
@@ -0,0 +1,31 @@
+//Cheese.java & StinkyCheese.java
+//Write a cheese class that includes a redefined default constructor and two methods, one of which is overloaded. Then write a stinky cheese class that extends the cheese class, but overwrites one of the cheese class' methods and also contains a new method just for stinky cheese.
+
+import java.util.*;
+
+public class Cheese {
+
+	public static void main(String[] args) { //not needed
+	
+		//edefined default constructor and two methods, one of which is overloaded.
+		
+		Cheese Cheddar = new Cheddar();
+		
+		static void Shredded (String cheddar); //overloaded methods have two or more parameters.
+			cheddar=shredded;
+			}
+			
+		static void Shredded(String colby) {
+			cheese=colby;
+			} //review
+			
+		static void Cut(String gouda) {
+			cut = gouda;
+			}
+		}}
+		
+	public class StinkyCheese {
+	
+	//stinky cheese
+	
+	}
---
+++ MagicBullet.java
@@ -0,0 +1,40 @@
+
+
+public class MagicBullet interface Blender{
+	
+	//use blender
+	*/
+	//method fruit, protein, milk
+		
+		//protected spinach, pulser;
+		
+		private ice, juice;
+		
+		
+		
+		void fruit(String strawberry) {
+			fruit = strawberry;
+			}
+			
+		void protein(String powder) {
+			protein = powder;
+			}
+			
+		void milk(String juice) {
+			milk = juice;
+			} //could go in with ice/juice
+			
+		void milk(String ice) {
+			milk = ice;
+			}
+		
+		
+		/*
+		
+	*/	String Combine (String Ingredients) {
+			combine = ingredients;
+			}
+		/*
+		
+		Blender
+		
